fc5585ab500d9f55516092449a02af16365b8543
Chigga0210 <calcam0210@gmail.com>
2026-01-21 16:42:52 -0600

    basis for database that everyone SHOULD be able to access
    created local capstone.db file, removed sql from routes.py, added sqlite into database.py for users table

--- backEnd/routes.py
+++ backEnd/routes.py
@@ -1,24 +1,9 @@
 from asyncio.windows_events import NULL
 from flask import Flask, render_template, request, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="../frontEnd/templates", static_folder="../frontEnd/static")
 
 app.secret_key = 'asdfasldgkjadkljaklsdjkljskldajksdjfkajsdkjafsdfasdf'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/Capstone'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-# to make an account
-class RegisterAccount(db.Model):
-    username = db.Column(db.String(100), primary_key = True)
-    password = db.Column(db.String(100))
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
 
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -36,10 +21,6 @@ def loginPage():
 def signupPage():
     return render_template("signup.html")
 
-# testing for adding database
-# @app.route('/', methods = ['GET'])
-# def getRegisterAccount():
-#     return jsonify({"Hello":"World"})
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='5000')
