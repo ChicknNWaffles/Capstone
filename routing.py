01bc3e5d9eaa46eb3a972ab14dd750c0fd5eaf0d
Fariza Abdurakhimova <144713777+fararahim@users.noreply.github.com>
2026-03-17 04:37:25 -0500

    Add files via upload
    A completely new file that configures the ws/terminal/ URL pathway to the backend terminal handler

--- routing.py
+++ routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from api.consumers import TerminalConsumer
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/terminal/$', TerminalConsumer.as_asgi()), # ADDED: Connects the 'ws/terminal/' URL to our Python terminal code
+    re_path(r'ws/terminal/$', consumers.TerminalConsumer.as_asgi()),
 ]
