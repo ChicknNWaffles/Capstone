5879a15425438066d924190868b381c98eae7fc1
JohnGagnon0 <jlg088@latech.edu>
2026-03-23 14:08:10 -0500

    Url Fix for CreateProject

--- Django/project/urls.py
+++ Django/project/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.getProjects.as_view(), name='GetProject'),
     path("getUserProjects", views.getUserProjects.as_view(), name='GetUserProjects'),
     path('create/', views.CreateProject.as_view(), name='CreateProject'),
+    path('createProject/', views.CreateProject.as_view(), name='CreateProjectAlt'),
     path('create2/', views.CreateProjectButWithSerializers.as_view(), name='CreateProjectButWithSerializers'),
 ]
