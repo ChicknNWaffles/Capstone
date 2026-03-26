665f58824003cc7017ec69571bffcbd4275095f5
Fariza <abfariza2002@gmail.com>
2026-03-26 01:31:43 -0500

    Implemented a file locking, renaming, and deletion functionality, enhance UI with new fonts and modal styles, and created a settings page

--- Django/Capstone/urls.py
+++ Django/Capstone/urls.py
@@ -35,6 +35,11 @@ def editorPage(request):
     template = loader.get_template("editor.html")
     return HttpResponse(template.render())
 
+# fariza's change: function to render the new settings page
+def settingsPage(request):
+    template = loader.get_template("settings.html")
+    return HttpResponse(template.render())
+
 
 urlpatterns = [
     # render pages
@@ -42,6 +47,8 @@ urlpatterns = [
     path("login/", loginPage),
     path("signup/", signupPage),
     path("editor/", editorPage),
+    # fariza's change: setting up the url path for the settings page
+    path("settings/", settingsPage),
 
     # admin pages
     path('admin/', admin.site.urls),
